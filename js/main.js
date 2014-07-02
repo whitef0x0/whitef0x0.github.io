043cee1dca94ae12cecb624cb443d02aa5283c79
Damian Wieczorek <damianw@umich.edu>
2014-07-02 01:47:46 -0400

    made colors matching background

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+colors = ['#B118AB', 
+          '#56264D', 
+          '#3A0122', 
+          '#422172', 
+          '#9a5305', 
+          '#3677c7', 
+          '#0d1d35', 
+          '#2b0111', 
+          '#082626', 
+          '#0e1a4a', 
+          '#190a0f', 
+          '#15111b', 
+          '#472942', 
+          '#860b04', 
+          '#552939', 
+          '#07211a', 
+          '#761634', 
+          '#440139', 
+          '#3c600a', 
+          '#360a09', 
+          '#2a3628'];
+
 // Document ready
 $(function() {
 
@@ -69,6 +91,7 @@ $(function() {
 
   // pick a random background
   var img = Math.floor(Math.random() * 21) + 1;
+  $('.colorified').css('background-color', colors[img-1]);
   var bg = '/img/poly/' + img +'.jpg';
   $('<img/>').attr('src', bg).load(function() {
     $(this).remove(); // prevent memory leaks as @benweet suggested
